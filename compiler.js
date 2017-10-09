73cebcd877c221cefbc8dee1d158e660a132d7fc
johncf <johncf@protonmail.com>
2017-10-09 12:45:00 +0530

    server: client queueing for compilation

--- compiler.js
+++ compiler.js
@@ -6,32 +6,49 @@ const nano = require('nanomsg');
 var worker = nano.socket('pair');
 worker.connect('tcp://127.0.0.1:8082');
 
-var clientQ = new UQueue();
+var clientQ = new UQueue(); // the front of the queue is the one getting compiled.
 var nextSeq = 1;
 
 worker.on('data', function(buf) {
   let msg = JSON.parse(buf);
-  let [id, data] = clientQ.peek();
-  if (msg.done) {
-    clientQ.deQ();
+  let qfront = clientQ.peek();
+  if (!qfront) {
+    console.error("Client queue empty on message!");
+    return;
   }
+  let [id, data] = qfront;
   if (data.seqnum == msg.seqnum) {
+    if (msg.done) {
+      clientQ.deQ();
+      compileNext();
+    }
     delete msg.seqnum;
     data.callback(msg);
   }
 });
 
+function compileNext() {
+  if (clientQ.length == 0) {
+    return;
+  }
+  let data = clientQ.peek()[1];
+  worker.send(JSON.stringify({
+    seqnum: data.seqnum,
+    source: data.source,
+    stdin: data.stdin
+  }));
+}
+
 module.exports.queue = function(clientId, source, callback, stdin=null) {
-  clientQ.enQ(clientId, { source: source,
-                          callback: callback,
-                          seqnum: nextSeq,
-                          stdin: stdin });
+  let data = {
+    seqnum: nextSeq,
+    source: source,
+    stdin: stdin,
+    callback: callback
+  };
+  clientQ.enQ(clientId, data);
   if (clientQ.length == 1) {
-    worker.send(JSON.stringify({
-      seqnum: nextSeq,
-      source: source,
-      stdin: stdin
-    }))
+    compileNext();
   }
   nextSeq += 1;
 }
